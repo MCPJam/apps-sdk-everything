34206172bf228184dd9d16623904557da4839cd3
Andrew Khadder <andrew.khadder@gmail.com>
2025-11-21 15:34:20 -0800

    conifg iframe fix?

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,19 @@ import { baseURL } from "./baseUrl";
 
 const nextConfig: NextConfig = {
   assetPrefix: baseURL,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-src 'self';"
+          }
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
