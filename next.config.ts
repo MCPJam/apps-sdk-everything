8b4d234a02a68a87b7e14b33a790a2a3fd40fe25
Andrew Khadder <andrew.khadder@gmail.com>
2025-11-21 15:39:13 -0800

    more config changes

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "frame-src 'self' https://apps-sdk-everything.vercel.app;"
+            value: "frame-src 'self' https://apps-sdk-everything.vercel.app; frame-ancestors *;"
           }
         ]
       }
